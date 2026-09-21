a16e8143cf5dbcb76b83573fd268d7fb36145c40
Sivakumar <sivakumarnatesh@gmail.com>
2026-09-21 20:29:59 +0530

    gh-pages installed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/student-management-FE/',
   server: {
     port: 3000,
     open: false,
